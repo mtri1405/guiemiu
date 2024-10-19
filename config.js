c79690cdc6504f57e4559b4bc45fc3223a02c424
mtri1405 <155385425+mtri1405@users.noreply.github.com>
2024-10-19 23:36:19 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Ngày hôm này là ngày phụ nữ Việt Nam 20.10 đó 😙',
+    desc: 'Công chúa của anh có muốn hôm nay anh chở đi chơi hem nè',
+    btnYes: 'Mình đi chơi anh nhe <33',
+    btnNo: 'Hông em lười rùi :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Chiều nay 4h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun a iu <3',
+    messLink: 'https://www.messenger.com/t/100011095451762' 
+}
